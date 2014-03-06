abc38d8db02d588aee9fc0e077302609e8963f7c
pierce-h <p.wayne@utexas.edu>
2014-03-06 15:09:52 -0600

    update railities

--- lib/apn/railtie.rb
+++ lib/apn/railtie.rb
@@ -2,11 +2,11 @@ module APN
   class Railtie < Rails::Railtie
     initializer "apn.setup" do |app|
 
-      APN.root = File.join(Rails.root, "config", "certs")
-      if Rails.env.development?
-        APN.certificate_name =  "apn_development.pem"
-        APN.host =  "gateway.sandbox.push.apple.com"
-      end
+      # APN.root = File.join(Rails.root, "config", "certs")
+      # if Rails.env.development?
+      #   APN.certificate_name =  "apn_development.pem"
+      #   APN.host =  "gateway.sandbox.push.apple.com"
+      # end
 
       logger = Logger.new(File.join(Rails.root, 'log', 'apn_sender.log'))
       APN.logger = logger
